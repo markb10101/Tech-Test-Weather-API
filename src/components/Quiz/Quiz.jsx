afa3394c9b095ae6f480b9d8b35ff626586ae3a5
Mark Birchall <mark.birchall@gmail.com>
2020-11-22 17:38:00 +0000

    quiz done

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import styles from "./Quiz.module.scss";
 
 const Quiz = () => {
+
+  const [userAnswer,setUserAnswer] = useState("");
+  const [reply, setReply] = useState("Waiting for your answer...");
+  const correctAnswer = "whippet";
+
+
+  const validateForm = (e) => {
+    e.preventDefault();
+    console.log("here");
+    
+    userAnswer.toLowerCase()==correctAnswer ? 
+      setReply("Yes, Whippet's are best!")
+      :
+      setReply("No, that's wrong I'm afraid.")
+    
+  }
+
+  useEffect(()=>{
+
+  },[reply])
+
+
   return (
-    <>
-      <p>Quiz works</p>
-    </>
+    <div className={styles.quiz}>
+      <form onSubmit={e => validateForm(e)}>
+        <label htmlFor="answer">
+          What's the best dog?
+        <input autocomplete="off" name="answer" placeholder="your answer" type="text" onChange={e => setUserAnswer(e.target.value)} required/>
+        </label>
+        <input type="submit" value="submit" onSubmit={e=>validateForm(e)}/>
+      </form>
+      <div className={styles.reply}>{reply}</div>
+    </div>
   );
 };
 
